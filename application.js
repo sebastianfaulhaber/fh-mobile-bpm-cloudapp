67627bea8f88f3fefddaf0476c6ff02288bbd29d
sfaulhab <sfaulhab@redhat.com>
2016-01-29 14:19:25 +0100

    Integrated mBaaS call.

--- application.js
+++ application.js
@@ -5,7 +5,7 @@ var cors = require('cors');
 
 // list the endpoints which you want to make securable here
 var securableEndpoints;
-securableEndpoints = ['/hello'];
+securableEndpoints = ['/bpm'];
 
 var app = express();
 
@@ -22,7 +22,7 @@ app.use(express.static(__dirname + '/public'));
 // Note: important that this is added just before your own Routes
 app.use(mbaasExpress.fhmiddleware());
 
-app.use('/hello', require('./lib/hello.js')());
+app.use('/bpm', require('./lib/bpm.js')());
 
 // Important that this is last!
 app.use(mbaasExpress.errorHandler());
@@ -30,7 +30,7 @@ app.use(mbaasExpress.errorHandler());
 var port = process.env.FH_PORT || process.env.OPENSHIFT_NODEJS_PORT || 8001;
 var host = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 app.listen(port, host, function() {
-  console.log("App started at: " + new Date() + " on port: " + port); 
+  console.log("App started at: " + new Date() + " on port: " + port);
 });
 
 //#############################################################################
@@ -41,18 +41,18 @@ app.listen(port, host, function() {
 var events = require('events');
 var submissionEventListener = new events.EventEmitter();
 
-submissionEventListener.on('submissionComplete', function(params){
+submissionEventListener.on('submissionComplete', function(params) {
   var submissionId = params.submissionId;
   var submissionCompletedTimestamp = params.submissionCompletedTimestamp;
   console.log("Submission with ID " + submissionId + " has completed at " + submissionCompletedTimestamp);
-  
+
   // Prepare params for REST call
   var myParamsForRESTCall = "";
-  for (var i=0; i<params.submission.formFields.length; i++) {
-      myParamsForRESTCall += "&map_";
-      myParamsForRESTCall += params.submission.formFields[i].fieldId.fieldCode;
-      myParamsForRESTCall += "=";
-      myParamsForRESTCall += params.submission.formFields[i].fieldValues[0];
+  for (var i = 0; i < params.submission.formFields.length; i++) {
+    myParamsForRESTCall += "&map_";
+    myParamsForRESTCall += params.submission.formFields[i].fieldId.fieldCode;
+    myParamsForRESTCall += "=";
+    myParamsForRESTCall += params.submission.formFields[i].fieldValues[0];
   }
   console.log("myParamsForRESTCall = " + myParamsForRESTCall);
 
@@ -68,29 +68,29 @@ submissionEventListener.on('submissionComplete', function(params){
       'Accept': 'application/json',
     }
   };
-  
+
   var req = http.request(options, function(res) {
     console.log('STATUS: ' + res.statusCode);
     console.log('HEADERS: ' + JSON.stringify(res.headers));
     res.setEncoding('utf8');
-    res.on('data', function (chunk) {
+    res.on('data', function(chunk) {
       console.log('BODY: ' + chunk);
     });
     res.on('end', function() {
       console.log('No more data in response.')
     })
   });
-  
+
   req.on('error', function(e) {
     console.log('Problem with request: ' + e.message);
   });
-  
+
   req.write("");
   req.end();
 
 });
 
-mbaasApi.forms.registerListener(submissionEventListener, function(err){
+mbaasApi.forms.registerListener(submissionEventListener, function(err) {
   if (err) return handleError(err);
   //submissionEventListener has now been registered with the $fh.forms Cloud API. Any valid Forms Events will now emit.
 });
