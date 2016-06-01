2814724828a2a2dadebcd60107e6c98532ba1b32
Sebastian Dehn <sifa@sifa-solutions.de>
2016-06-01 19:12:54 +0200

    Changed mBasS GUID

--- Gruntfile.js
+++ Gruntfile.js
@@ -55,7 +55,7 @@ module.exports = function(grunt) {
            * or a remote instance.
            */
           var serviceMap = {
-            '3m3gujrwux6jo6fjx44npd35': 'http://127.0.0.1:8001',
+            'wbyyx52jcku3ynnqywa666ry': 'http://127.0.0.1:8001',
             'SERVICE_GUID_2': 'https://host-and-path-to-service'
           };
           return JSON.stringify(serviceMap);
